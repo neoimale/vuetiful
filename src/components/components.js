6f4e385082aa6394e1e9f99da106f3eb327009d6
Neo Shan <neoimale@gmail.com>
2017-05-10 11:12:25 +0800

    remove some components

--- src/components/components.js
+++ src/components/components.js
@@ -4,16 +4,16 @@ import Checkbox from "./toggles/checkbox.vue";
 import Chip from "./chip/chip.vue";
 import DataTable from "./datatable/datatable.vue";
 import DataTableColumn from "./datatable/datatable-column.vue";
-import DateTimePicker from "./datetime-picker/datetime-picker.vue";
+// import DateTimePicker from "./datetime-picker/datetime-picker.vue";
 import Dynamic from "./dynamic/dynamic";
 import FloatingPanel from "./floating-panel/floating-panel.vue";
-import Modal from "./modal/modal.vue";
+// import Modal from "./modal/modal.vue";
 import Paginator from "./paginator/paginator.vue";
 import Panel from "./panel/panel.vue";
-import Radio from "./toggles/radio.vue";
-import TabControl from "./tab-control/tab-control.vue";
-import TabPane from "./tab-control/tab-pane.vue";
-import Toggle from "./toggles/toggle.vue";
+// import Radio from "./toggles/radio.vue";
+// import TabControl from "./tab-control/tab-control.vue";
+// import TabPane from "./tab-control/tab-pane.vue";
+// import Toggle from "./toggles/toggle.vue";
 
 export default {
     calendar: Calendar,
@@ -21,14 +21,14 @@ export default {
     chip: Chip,
     datatable: DataTable,
     datatableColumn: DataTableColumn,
-    datetimePicker: DateTimePicker,
+    // datetimePicker: DateTimePicker,
     dynamic: Dynamic,
     floatingPanel: FloatingPanel,
-    modal: Modal,
+    // modal: Modal,
     paginator: Paginator,
     panel: Panel,
-    radio: Radio,
-    toggle: Toggle,
-    tabControl: TabControl,
-    tabPane: TabPane
+    // radio: Radio,
+    // toggle: Toggle,
+    // tabControl: TabControl,
+    // tabPane: TabPane
 }
